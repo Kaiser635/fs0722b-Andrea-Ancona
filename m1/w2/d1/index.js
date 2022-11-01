2416797d0184d29703d79bc4c9d6f47159e69cb0
Kaiser635 <a-ancona@libero.it>
2022-11-01 10:39:14 +0100

    provaw2

--- m1/w2/d1/index.js
+++ m1/w2/d1/index.js
@@ -1,3 +1,16 @@
-alert('Benvenuti nel mio Sito..!!');
+alert('Controlliamo se puoi bere!!')
 
-alert('Inserisci i tuoi dati');
+
+
+
+function drink() {
+    x = document.getElementById('ageinput').value;//var. che prende il valore inserito in input
+
+    
+    //funzione if
+    if (x < 18){
+        document.getElementById('ageoutput').innerHTML = "Tu non puoi bere..!!"//risultato nel div con id=output
+    }else{
+        document.getElementById('ageoutput').innerHTML = "Tu puoi bere e ubriacarti..!!"
+    }
+}
